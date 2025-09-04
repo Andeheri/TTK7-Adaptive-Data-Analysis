28862c6520b95278c093d4446e0e764e9e60f36d
Anders Haarberg Eriksen <anderseriksen02@gmail.com>
2025-09-04 09:24:48 +0200

    Added the later stages with offset and white noise

--- group_work_1/main.py
+++ group_work_1/main.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-def plot_fft(signal: np.ndarray, fs: int):
+def plot_fft(signal: np.ndarray, fs: int, tag: str | None = None):
     n = len(signal)
     f = np.fft.rfftfreq(n, d=1/fs)
     fft_values = np.fft.rfft(signal)
@@ -14,26 +14,31 @@ def plot_fft(signal: np.ndarray, fs: int):
     plt.plot(f, magnitude)
     plt.xlabel("Frequency [Hz]")
     plt.ylabel("Magnitude")
-    plt.title("FFT of the Signal")
+    plt.title(f"FFT of the Signal {f'with {tag}' if tag else ''}")
     plt.grid()
-    plt.savefig("group_work_1/plots/fft_plot.png", dpi=300)
+    plt.savefig(f"group_work_1/plots/fft_plot{'_' + '_'.join(tag.split(' ')) if tag else ''}.png", dpi=300)
     plt.show(block = False)
 
 
-def plot_signal(t: np.ndarray, signal: np.ndarray):
+def plot_signal(t: np.ndarray, signal: np.ndarray, tag: str | None = None):
     plt.figure()
     plt.plot(t, signal * 1e6)  # Convert to microvolts
     plt.xlabel("Time [s]")
     plt.ylabel("Amplitude [$\\mu V$]")
-    plt.title("Signal in Time Domain")
+    plt.title(f"Signal {f'with {tag}' if tag else ''} in Time Domain")
     plt.grid()
-    plt.savefig("group_work_1/plots/signal_plot.png", dpi=300)
+    plt.savefig(f"group_work_1/plots/signal_plot{'_' + '_'.join(tag.split(' ')) if tag else ''}.png", dpi=300)
     plt.show(block = False)
 
 
 def main():
     filepath = Path("group_work_1/Signal1_2018.csv")
     fs = 100  # Sampling frequency
+    # Offset parameters
+    offset_amplitude = 1e-6  # 1 microvolt
+    # Noise parameters
+    mu = 0  # Mean
+    sigma = 5e-6  # Standard deviation (5 microvolts)
 
     if not filepath.exists():
         print(f"File {filepath} does not exist.")
@@ -42,8 +47,21 @@ def main():
     signal = np.loadtxt(filepath, delimiter=",")
     t = np.arange(len(signal)) / fs  # Time vector
 
+    # Raw signal
     plot_signal(t, signal)
     plot_fft(signal, fs)
+
+    # Adding offset
+    signal_with_offset = signal + offset_amplitude  # Adding DC offset
+    plot_signal(t, signal_with_offset, tag="offset")
+    plot_fft(signal_with_offset, fs, tag="offset")
+
+    # Adding noise
+    signal_with_noise = signal + np.random.normal(mu, sigma, size=signal.shape)  # Adding white noise
+
+    plot_signal(t, signal_with_noise, tag="white noise")
+    plot_fft(signal_with_noise, fs, tag="white noise")
+
 
 if __name__ == "__main__":
     main()
